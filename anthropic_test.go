c62363600e88e4c3af2f6c6048494c50fe1e64dc
Mitar <mitar.git@tnode.com>
2025-02-25 14:55:18 +0100

    Fix test.

--- anthropic_test.go
+++ anthropic_test.go
@@ -14,17 +14,18 @@ func TestAnthropicJSON(t *testing.T) {
 	t.Parallel()
 
 	provider := fun.AnthropicTextProvider{
-		Client:            nil,
-		APIKey:            "xxx",
-		Model:             "claude-3-haiku-20240307",
-		MaxContextLength:  43,
-		MaxResponseLength: 56,
-		MaxExchanges:      57,
-		PromptCaching:     true,
-		Temperature:       0.7,
+		Client:                 nil,
+		APIKey:                 "xxx",
+		Model:                  "claude-3-haiku-20240307",
+		MaxContextLength:       43,
+		MaxResponseLength:      56,
+		MaxExchanges:           57,
+		PromptCaching:          true,
+		ExtendedThinkingBudget: 12345,
+		Temperature:            0.7,
 	}
 
 	out, errE := x.MarshalWithoutEscapeHTML(provider)
 	require.NoError(t, errE, "% -+#.1v", errE)
-	assert.Equal(t, `{"type":"anthropic","model":"claude-3-haiku-20240307","maxContextLength":43,"maxResponseLength":56,"maxExchanges":57,"promptCaching":true,"temperature":0.7}`, string(out)) //nolint:testifylint
+	assert.Equal(t, `{"type":"anthropic","model":"claude-3-haiku-20240307","maxContextLength":43,"maxResponseLength":56,"maxExchanges":57,"promptCaching":true,"extendedThinkingBudget":12345,"temperature":0.7}`, string(out)) //nolint:testifylint
 }
