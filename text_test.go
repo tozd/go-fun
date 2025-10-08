c0fbc79e885189c9eec0506c945d528eddfbb399
Mitar <mitar.git@tnode.com>
2025-10-08 14:02:39 +0200

    Revert "Use GPT OSS."
    This reverts commit 1e01c48f42bd9198f912485ee9ffee142249d9c9.

--- text_test.go
+++ text_test.go
@@ -115,7 +115,7 @@ var providers = []testProvider{
 			return &fun.OllamaTextProvider{
 				Client:            nil,
 				Base:              os.Getenv("OLLAMA_HOST"),
-				Model:             "gpt-oss:20b",
+				Model:             "qwen3:32b",
 				MaxContextLength:  0,
 				MaxResponseLength: 0,
 				Seed:              42,
@@ -227,7 +227,7 @@ var providersForTools = []testProvider{
 			return &fun.OllamaTextProvider{
 				Client:            nil,
 				Base:              os.Getenv("OLLAMA_HOST"),
-				Model:             "gpt-oss:20b",
+				Model:             "qwen3:32b",
 				MaxContextLength:  0,
 				MaxResponseLength: 0,
 				Seed:              42,
