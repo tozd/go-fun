1e01c48f42bd9198f912485ee9ffee142249d9c9
Mitar <mitar.git@tnode.com>
2025-10-08 13:42:57 +0200

    Use GPT OSS.

--- text_test.go
+++ text_test.go
@@ -115,7 +115,7 @@ var providers = []testProvider{
 			return &fun.OllamaTextProvider{
 				Client:            nil,
 				Base:              os.Getenv("OLLAMA_HOST"),
-				Model:             "qwen3:32b",
+				Model:             "gpt-oss:20b",
 				MaxContextLength:  0,
 				MaxResponseLength: 0,
 				Seed:              42,
@@ -227,7 +227,7 @@ var providersForTools = []testProvider{
 			return &fun.OllamaTextProvider{
 				Client:            nil,
 				Base:              os.Getenv("OLLAMA_HOST"),
-				Model:             "qwen3:32b",
+				Model:             "gpt-oss:20b",
 				MaxContextLength:  0,
 				MaxResponseLength: 0,
 				Seed:              42,
